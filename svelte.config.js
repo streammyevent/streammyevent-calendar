35af9a78abcb9d325203133c61860c3e773e13d0
Floris Porro <floris@streammyevent.com>
2025-06-04 14:37:58 +0200

    Force inlining bundle

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		output: {
+			bundleStrategy: 'inline'
+		},
+	},
 };
 
 export default config;
